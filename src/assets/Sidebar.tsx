870a19614e97c585f27ee6c722a80bad3522113f
sabrina.zancotti <sabrina.zancotti@netclean.com>
2024-04-23 16:13:29 +0200

    Added a container for the sidebar

---
+++ src/assets/Sidebar.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Sidebar = () => {
+  return <Container>Components</Container>;
+};
+
+export default Sidebar;
+
+const Container = styled.div`
+  background-color: #e4e7f2;
+  width: 20%;
+  flex-grow: 1;
+`;
